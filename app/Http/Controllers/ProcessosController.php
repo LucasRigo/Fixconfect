1d41a7dddf29c2e76ff395d4aa21e84367f38965
Lucas Rigo <183015@upf.br>
2022-09-20 23:20:14 -0300

    tabelas

--- app/Http/Controllers/ProcessosController.php
+++ app/Http/Controllers/ProcessosController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ProcessosController extends Controller
 {
-    //
+    public function index(){
+        $nome = "teste";
+        return view('processos', ['nome'=>$nome]);
+    }
 }
--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Terceirizado extends Model
+class Pagamento extends Model
 {
     use HasFactory;
 }
